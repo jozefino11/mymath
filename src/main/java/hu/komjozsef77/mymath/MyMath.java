cef86ce2ddec7dfaa5349782945a9f8b0e7aae3e
Jozsef Komaromi <komjozsef77@gmail.com>
2025-04-20 11:42:01 +0200

    Initial commit

---
+++ src/main/java/hu/komjozsef77/mymath/MyMath.java
@@ -0,0 +1,8 @@
+package hu.komjozsef77.mymath;
+
+public class MyMath {
+	public static int myAdd(int a, int b) {
+		return a+b;
+	}
+	}
+
---
+++ src/test/java/hu/komjozsef77/mymath/MyMathTest.java
@@ -0,0 +1,13 @@
+package hu.komjozsef77.mymath;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MyMathTest {
+@Test
+public void testAddPositive() {
+assertEquals(5, MyMath.myAdd(3, 2));
+}
+
+}
